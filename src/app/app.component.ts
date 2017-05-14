b66f4241df1d497e636425b208a760e1c43d8103
okapie <tatigreuze@yahoo.co.jp>
2017-05-14 12:08:46 +0900

    [add] Added todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TodoStore, Todo} from './store';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  todoStore: TodoStore;
+  newTodoText = '';
+
+  constructor(todoStore: TodoStore) {
+    this.todoStore = todoStore;
+  }
+
+  stopEditing(todo: Todo, editedTitle: string) {
+    if(editedTitle.length !== 0){
+      todo.title = editedTitle.trim();
+    }
+    todo.editing = false;
+  }
+
+  updateEditingTodo(todo: Todo, editedTitle: string) {
+    editedTitle = editedTitle.trim();
+    todo.editing = false;
+
+    if (editedTitle.length === 0) {
+      return this.todoStore.remove(todo);
+    }
+
+    todo.title = editedTitle;
+  }
+
+  editTodo(todo: Todo) {
+    todo.editing = true;
+  }
+
+  removeCompleted() {
+    this.todoStore.removeCompleted();
+  }
+
+  toggleCompletion(todo: Todo) {
+    this.todoStore.toggleCompletion(todo);
+  }
+
+  remove(todo: Todo){
+    this.todoStore.remove(todo);
+  }
+
+  addTodo() {
+    if (this.newTodoText.trim().length) {
+      this.todoStore.add(this.newTodoText);
+      this.newTodoText = '';
+    }
+  }
 }
